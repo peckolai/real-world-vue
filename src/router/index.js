971022519770619d15f575dcaf3cf0f71664937b
peckolai <peckolai@gmail.com>
2021-12-27 15:33:46 +0200

    Initialize

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,23 @@
-import { createRouter, createWebHistory } from "vue-router";
-import EventList from "../views/EventList.vue";
-import About from "../views/About.vue"
+import { createRouter, createWebHistory } from 'vue-router';
+import EventList from '../views/EventList.vue';
+import EventDetails from '../components/EventDetails.vue';
+import About from '../views/About.vue';
 
 const routes = [{
-        path: "/",
-        name: "EventList",
+        path: '/',
+        name: 'EventList',
         component: EventList,
     },
     {
-        path: "/about",
-        name: "About",
-        component: About
-
+        path: '/event/:id',
+        name: 'EventDetails',
+        props: true,
+        component: EventDetails,
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About,
     },
 ];
 
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -8,6 +8,7 @@
 <script>
 // @ is an alias to /src
 import EventCard from '@/components/EventCard.vue';
+import EventService from '@/services/EventService.js';
 
 export default {
   name: 'EventList',
@@ -16,8 +17,17 @@ export default {
   },
   data() {
     return {
-      
+      events: null,
     };
+  },
+  created() {
+    EventService.getEvents()
+      .then((response) => {
+        this.events = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
 };
 </script>
